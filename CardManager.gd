b756b7776fc0d8d87923d0dcd5ca14c0c8fea4ef
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2021-12-23 01:01:35 -0500

    Added new cards

--- CardManager.gd
+++ CardManager.gd
@@ -11,21 +11,33 @@ const sigils := {
 
 
 func _ready() -> void:
-	var child := Card.new("The Child", 2, 2, 2, 2)
+	var child := Card.new("The Child", 2, 2, 2, 3)
 	child.image = preload("res://CardParts/CardImages/Child.png")
 	child.hability_one = "molochian"
 	child.evolve = "Scared Child"
 	cards.append(child)
 	
-	var scared_child := Card.new("Scared Child", 4, 3, 2, 2)
+	var scared_child := Card.new("Scared Child", 4, 3, 2, 3)
 	scared_child.image = preload("res://CardParts/CardImages/ScaredChild.png")
 	scared_child.hability_one = "molochian"
 	scared_child.require = "The Child"
 	cards.append(scared_child)
 	
-	var cat := Card.new("Cat", 2, 1, 2, 1)
+	var cat := Card.new("Cat", 2, 1, 1, 2)
 	cat.image = preload("res://CardParts/CardImages/Cat.png")
 	cards.append(cat)
+	
+	var dog := Card.new("Dog", 1, 1, 1, 1)
+	dog.image = preload("res://CardParts/CardImages/Dog.png")
+	cards.append(dog)
+	
+	var mosquito := Card.new("Mosquito", 1, 1, 1, 1)
+	mosquito.image = preload("res://CardParts/CardImages/Mosquito.png")
+	cards.append(mosquito)
+	
+	var centi := Card.new("Centipede", 1, 1, 2, 1)
+	centi.image = preload("res://CardParts/CardImages/Centipede.png")
+	cards.append(centi)
 
 
 func get_sigil_texture(n:String) -> Texture:
--- Game.gd
+++ Game.gd
@@ -1,10 +1,6 @@
 extends Node2D
 
 class PlayerState:
-	var hand := []
-	var blood := 0
-	var coins := 0
-class AIState:
 	var lane_knowledge := [null, null, null, null]
 	var hand := []
 	var blood := 0
@@ -16,7 +12,7 @@ const LaneDisplay := preload("res://LaneButton.tscn")
 var card_in_hand :Card = null
 var player_state := PlayerState.new()
 
-var ai_state := AIState.new()
+var ai_state := PlayerState.new()
 
 var player_lanes := []
 var bot_lanes := []
@@ -29,7 +25,12 @@ var player_turn := true
 func _ready() -> void:
 	randomize()
 	var c := LaneDisplay.instance()
-	
+	for i in health:
+		var a := Sprite.new()
+		a.texture = preload("res://Coin.png")
+		a.position.y = CardManager.card_size.y + 4 + 60 + randf() * 100 - 50
+		a.position.x = CardManager.card_size.x * 4 + 16 + 60 + 50 + randf() * 100
+		$Pool.add_child(a)
 	for i in 4:
 		c = LaneDisplay.instance()
 		c.position.y = CardManager.card_size.y + 4 + 60
@@ -46,13 +47,9 @@ func _ready() -> void:
 		bot_lanes.append(c)
 		add_child(c)
 	
-	for i in 3:
+	for i in 5:
 		player_state.hand.append(CardManager.cards[randi()%CardManager.cards.size()].duplicate())
 		ai_state.hand.append(CardManager.cards[randi()%CardManager.cards.size()].duplicate())
-	for i in 2:
-		player_state.hand.append(CardManager.cards[2].duplicate())
-		ai_state.hand.append(CardManager.cards[2].duplicate())
-	
 	
 	
 	player_state.hand.shuffle()
@@ -60,19 +57,24 @@ func _ready() -> void:
 
 
 func _on_lane_clicked(object:Node2D) -> void:
+	var whostate := player_state
+	if not player_turn:
+		whostate = ai_state
 	if card_in_hand != null:
 		if object.card == null and card_in_hand.require == "":
 			object.card = card_in_hand
 			if object.card.hability_one == "blood_flask":
-				player_state.blood += 1
-			player_state.blood -= card_in_hand.cost
+				whostate.blood += 1
+			whostate.blood -= card_in_hand.cost
 			card_in_hand = null
 		elif object.card != null and card_in_hand.require == object.card.name:
 			object.card = card_in_hand
-			player_state.blood -= card_in_hand.cost
+			whostate.blood -= card_in_hand.cost
 			card_in_hand = null
 			if object.card.hability_one == "blood_flask":
-				player_state.blood += 1
+				whostate.blood += 1
+	if object.card != null and object.card.name == "Blood Flask" and card_in_hand == null and Input.is_key_pressed(KEY_SHIFT):
+		object.card = null
 	object.update_card()
 
 
@@ -81,7 +83,10 @@ func _input(event: InputEvent) -> void:
 		if event.pressed:
 			if event.button_index == 2:
 				if card_in_hand != null:
-					player_state.hand.append(card_in_hand)
+					var whostate := player_state
+					if not player_turn:
+						whostate = ai_state
+					whostate.hand.append(card_in_hand)
 					card_in_hand = null
 					update_hand()
 			elif event.button_index == 3:
@@ -123,34 +128,54 @@ func _input(event: InputEvent) -> void:
 						player_state.coins += blc[i].strength
 						health -= blc[i].strength
 				player_turn = !player_turn
+				update_hand()
 				
-				if !player_turn:
-					var played := false
-					var p = bot_lanes[randi()%4]
-					if ai_state.blood < 1:
-						var rand_card :Card = CardManager.make_blood_flask()
-						p.card = rand_card
-					p.update_card()
-					while played == false:
-						p = bot_lanes[randi()%4]
-						var rand_card :Card = ai_state.hand[randi()%ai_state.hand.size()]
-						if rand_card != null:
-							if p.card == null and rand_card.require == "":
-								p.card = rand_card
-								if p.card.hability_one == "blood_flask":
-									ai_state.blood += 1
-								ai_state.blood -= rand_card.cost
-								rand_card = null
-								played = true
-							elif p.card != null and rand_card.require == p.card.name:
-								p.card = rand_card
-								ai_state.blood -= rand_card.cost
-								rand_card = null
-								if p.card.hability_one == "blood_flask":
-									ai_state.blood += 1
-								played = true
-					ai_state.hand.append(CardManager.cards[randi()%CardManager.cards.size()].duplicate())
-					p.update_card()
+#				if !player_turn:
+#					var played := false
+#					var p = bot_lanes[randi()%4]
+#					if ai_state.blood < 1:
+#						var rand_card :Card = CardManager.make_blood_flask()
+#						var done := false
+#						var tries := 0
+#						while not done and tries < 25:
+#							tries += 1
+#							if p.card == null:
+#								done = true
+#								p.card = rand_card
+#							p = bot_lanes[randi()%4]
+#					p.update_card()
+#					var tries := 0
+#					while played == false and tries < 25:
+#						tries += 1
+#						p = bot_lanes[randi()%4]
+#						var rand_card :Card = ai_state.hand[randi()%ai_state.hand.size()]
+#						if rand_card != null:
+#							if p.card == null and rand_card.require == "":
+#								p.card = rand_card
+#								if p.card.hability_one == "blood_flask":
+#									ai_state.blood += 1
+#								ai_state.blood -= rand_card.cost
+#								rand_card = null
+#								played = true
+#							elif p.card != null and rand_card.require == p.card.name:
+#								p.card = rand_card
+#								ai_state.blood -= rand_card.cost
+#								rand_card = null
+#								if p.card.hability_one == "blood_flask":
+#									ai_state.blood += 1
+#								played = true
+#					if not played:
+#						var rand_card :Card = CardManager.make_blood_flask()
+#						var done := false
+#						tries = 0
+#						while not done and tries < 25:
+#							tries += 1
+#							if p.card == null:
+#								done = true
+#								p.card = rand_card
+#							p = bot_lanes[randi()%4]
+#					ai_state.hand.append(CardManager.cards[randi()%CardManager.cards.size()].duplicate())
+#					p.update_card()
 				
 				for i in 4:
 					if player_lanes[i].card:
@@ -162,14 +187,23 @@ func _input(event: InputEvent) -> void:
 						
 					bot_lanes[i].update_card()
 					player_lanes[i].update_card()
-				print(health, " ", player_state.coins, " ", ai_state.coins)
+				update_hand()
+				print(health, " | ", player_state.blood, " ", ai_state.blood)
+				if player_turn:
+					print("Down")
+				else:
+					print("Up")
+				update_pool_health()
 
 
 func update_hand():
 	for i in $Hand.get_children():
 		i.queue_free()
 	var count := 0
-	for i in player_state.hand:
+	var whostate := player_state
+	if not player_turn:
+		whostate = ai_state
+	for i in whostate.hand:
 		var cd := CardDisplay.instance()
 		cd.card = i
 		cd.update_card()
@@ -181,21 +215,56 @@ func update_hand():
 
 
 func _on_DeckButton_pressed() -> void:
-	if player_turn:
-		var randcard :Card = CardManager.cards[randi()%CardManager.cards.size()].duplicate()
-		player_state.hand.append(randcard)
-		update_hand()
+	var whostate := player_state
+	if not player_turn:
+		whostate = ai_state
+	var randcard :Card = CardManager.cards[randi()%CardManager.cards.size()].duplicate()
+	whostate.hand.append(randcard)
+	update_hand()
 
 
 func _on_HandButton_pressed(card_number) -> void:
-	if player_turn:
-		if card_in_hand == null and player_state.hand[card_number].cost <= player_state.blood:
-			card_in_hand = player_state.hand[card_number]
-			player_state.hand.remove(card_number)
-		update_hand()
+	var whostate := player_state
+	if not player_turn:
+		whostate = ai_state
+	if card_in_hand == null and whostate.hand[card_number].cost <= whostate.blood:
+		card_in_hand = whostate.hand[card_number]
+		whostate.hand.remove(card_number)
+	update_hand()
 
 
 func _on_BloodDeckButton_pressed() -> void:
-	if player_turn:
-		player_state.hand.append(CardManager.make_blood_flask())
-		update_hand()
+	var whostate := player_state
+	if not player_turn:
+		whostate = ai_state
+	whostate.hand.append(CardManager.make_blood_flask())
+	update_hand()
+
+
+func update_pool_health():
+	var a := $Players1.get_child_count()
+	var b := $Players2.get_child_count()
+	for i in player_state.coins - a:
+		if i < $Pool.get_child_count():
+			var c := $Pool.get_child(i)
+			var s := Sprite.new()
+			s.texture = preload("res://Coin.png")
+			s.position = c.position
+			$Players1.add_child(s)
+			c.queue_free()
+			var twee := Tween.new()
+			twee.interpolate_property(s, "position", s.position, s.position + Vector2(randf() * 20 - 10, 150 + randf() * 20 - 10), 0.5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+			add_child(twee)
+			twee.start()
+	for i in ai_state.coins - b:
+		if i < $Pool.get_child_count():
+			var c := $Pool.get_child(i)
+			var s := Sprite.new()
+			s.texture = preload("res://Coin.png")
+			s.position = c.position
+			$Players2.add_child(s)
+			c.queue_free()
+			var twee := Tween.new()
+			twee.interpolate_property(s, "position", s.position, s.position + Vector2(randf() * 20 - 10, -150 + randf() * 20 - 10), 0.5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+			add_child(twee)
+			twee.start()
